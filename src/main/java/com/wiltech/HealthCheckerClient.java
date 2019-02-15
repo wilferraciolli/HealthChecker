959662fea1111d5cb41419a9161b156130a8474e
Wiliam Ferraciolli <wiliam334@hotmail.com>
2019-02-15 17:48:52 +0000

    initial commit

---
+++ src/main/java/com/wiltech/HealthCheckerClient.java
@@ -0,0 +1,14 @@
+/*
+ * (c) Midland Software Limited 2019
+ * Name     : HealthCheckerClient.java
+ * Author   : ferraciolliw
+ * Date     : 15 Feb 2019
+ */
+package com.wiltech;
+
+/**
+ *
+ */
+@Stateless
+public class HealthCheckerClient {
+}
